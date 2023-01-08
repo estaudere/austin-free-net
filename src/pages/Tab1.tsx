9f64df3c4df761e661b89583e25e50d9d181bc2d
alisha-murty <alisha_murty@yahoo.com>
2023-01-07 22:51:43 -0600

    added about page

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -5,6 +5,7 @@ import { initializeApp } from "firebase/app";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { getFirestore } from "firebase/firestore";
 import './Tab1.css';
+import afn from './AFN-pic.png'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -37,7 +38,7 @@ const Tab1: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>About</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -46,8 +47,41 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">News</IonTitle>
           </IonToolbar>
         </IonHeader>
-        {data.map((doc:any) => <div>{doc.title}</div>)}
-        <ExploreContainer name="Tab 1 page" />
+        <img src={afn} />
+        {/* {data.map((doc:any) => <div>{doc.title}</div>)} */}
+        {/* <ExploreContainer name="Tab 1 page2" /> */}        
+        <h1 style={{color: '#6aa2fc', paddingLeft: 15}}>Our Story</h1>
+        <h3 style={{paddingLeft: 15, paddingRight: 10}}>Incorporated in 1995 by legendary community leaders, Sue Beckwith and Ana Sisnett (1952-2009), Austin Free-Net was the first organization to wire city libraries and put computers in schools and community centers around Austin. Since our founding, the need for digital skills and access to technology has only grown stronger.</h3>
+        <h3 style={{paddingLeft: 15, paddingRight: 10}}>Austin Free-Net is focused on breaking down barriers to access and education by continuing to manage integral public access computer labs throughout the city. These public access computer labs offer a variety of basic digital skills education classes and workshops in both physical and virtual environments. Additionally, we organize device donation procurement in partnership with the City of Austin's Community PC Program which directly benefits community members and organizations who are in need of computer equipment. Our Digital Navigator program, which launched in 2021, meets the demand of the community's desire for one on one basic digital assistance to affordable internet access, device acquisition, technical skills, communication and application support.</h3>
+        <h3 style={{paddingLeft: 15, paddingRight: 10}}>For over 26 years and counting, we continue to carry on Ana Sisnett's legacy by working with the community to provide digital access and education to all with a heavy emphasis on our most underserved community members.</h3>
+        <br></br>
+        <br></br>
+        <h1 style={{color: '#6aa2fc', paddingLeft: 15}}>Donate</h1>
+        <h3 style={{paddingLeft: 15, paddingRight: 10}}>Your contribution will directly impact bridging the digital divide and helping someone excel in the digital age. Thank you for your support of everything Austin Free-Net is doing! All gifts are 100% tax deductible.</h3>
+        <br></br>
+        <a style={{paddingLeft: 50}} href="https://www.paypal.com/donate/?cmd=_s-xclick&hosted_button_id=HPV6ZMGRW5VWU">
+          <button style={{ backgroundColor: "#6aa2fc", width: "350px", height: "50px", color: "black", fontSize: "20px" }}>DONATE VIA PAYPAL</button>
+        </a>
+        <br></br>
+        <br></br>
+        <a style={{paddingLeft: 50}} href="https://givebutter.com/austin-free-net">
+          <button style={{ backgroundColor: "#6aa2fc", width: "350px", height: "50px", color: "black", fontSize: "20px" }}>DONATE VIA VENMO</button>
+        </a>
+        <br></br>
+        <br></br>
+        <a style={{paddingLeft: 50}} href="https://givebutter.com/austin-free-net">
+          <button style={{ backgroundColor: "#6aa2fc", width: "350px", height: "50px", color: "black", fontSize: "20px" }}>DONATE VIA CARD</button>
+        </a>
+        <br></br>
+        <br></br>
+        <h1 style={{color: '#6aa2fc', paddingLeft: 15}}>Volunteer</h1>
+        <h3 style={{paddingLeft: 15, paddingRight: 10}}>Can you send an e-mail? Do you like lending a hand? Do you have experience in a technology career? Any of these items qualifies you to volunteer! Volunteers are our lifeblood and there are many ways that you can help.</h3>
+        <br></br>
+        <a style={{paddingLeft: 50}} href="https://docs.google.com/forms/d/e/1FAIpQLSfRf-BVusPsC3NcPw-cAImhSRy1MImOV1IIZLAqFyDCQhLJQw/viewform?vc=0&amp;c=0&amp;w=1">
+          <button style={{ backgroundColor: "#6aa2fc", width: "350px", height: "50px", color: "black", fontSize: "20px" }}>VOLUNTEER APPLICATION FORM</button>
+        </a>
+        <br></br>
+        <br></br>
       </IonContent>
     </IonPage>
   );
